f007d1f1a599fa56e9121d682915b575376b795b
andrei.panas <panas.andreww@gmail.com>
2021-05-31 10:40:19 +0300

    update service layer

--- src/main/java/com/leverx/pets/controller/servlet/PersonServlet.java
+++ src/main/java/com/leverx/pets/controller/servlet/PersonServlet.java
@@ -3,7 +3,6 @@ package com.leverx.pets.controller.servlet;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.leverx.pets.model.Person;
 import com.leverx.pets.service.PersonService;
-import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.ServletContext;
 import javax.servlet.annotation.WebServlet;
@@ -12,10 +11,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
-@WebServlet("/people/*")
+import static com.leverx.pets.util.JsonUtil.readJsonData;
+import static com.leverx.pets.util.JsonUtil.sendJsonResponse;
+import static com.leverx.pets.util.UrlParser.*;
+
+@WebServlet(value = "/people/*", name = "PersonServlet")
 public class PersonServlet extends HttpServlet {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
@@ -29,48 +31,31 @@ public class PersonServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String pathInfo = request.getPathInfo();
-        if (pathInfo == null || pathInfo.equals("/")){
+        List<String> url = getPathInfo(request);
+        if (url == null) {
             List<Person> people = personService.getAllPeople();
-
-            response.setContentType("application/json");
-            response.setCharacterEncoding("UTF-8");
             String jsonPeople = objectMapper.writeValueAsString(people);
-            PrintWriter out = response.getWriter();
-            out.print(jsonPeople);
-            out.flush();
+            sendJsonResponse(jsonPeople, response);
         }
-        else {
-            String[] endpoints = pathInfo.split("/");
-            if (endpoints.length != 2) {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-            }
-            else if (StringUtils.isNumeric(endpoints[1])) {
-                Person person = personService.getPersonById(Long.parseLong(endpoints[1]));
-                if (person != null) {
-                    response.setContentType("application/json");
-                    response.setCharacterEncoding("UTF-8");
-                    String jsonPerson = objectMapper.writeValueAsString(person);
-
-                    PrintWriter out = response.getWriter();
-                    out.print(jsonPerson);
-                    out.flush();
-                }
-                else {
-                    response.sendError(HttpServletResponse.SC_NOT_FOUND);
-                }
+        else if (url.size() == 1) {
+            Person person = personService.getPersonById(Long.parseLong(url.get(0)));
+            if (person != null) {
+                String jsonPerson = objectMapper.writeValueAsString(person);
+                sendJsonResponse(jsonPerson, response);
             }
             else {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
             }
+        }
+        else {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
         }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String pathInfo = request.getPathInfo();
-
-        if (pathInfo == null || pathInfo.equals("/")){
+        List<String> url = getPathInfo(request);
+        if (url == null) {
             BufferedReader reader = request.getReader();
             String json = readJsonData(reader);
             Person person = objectMapper.readValue(json, Person.class);
@@ -84,62 +69,41 @@ public class PersonServlet extends HttpServlet {
 
     @Override
     protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String pathInfo = request.getPathInfo();
-        if (pathInfo != null) {
-            String[] endpoints = pathInfo.split("/");
-            if (endpoints.length != 2) {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-            }
-            else if (StringUtils.isNumeric(endpoints[1])) {
-                BufferedReader reader = request.getReader();
-                String json = readJsonData(reader);
-                Person newPerson = objectMapper.readValue(json, Person.class);
-                Person person = personService.getPersonById(Long.parseLong(endpoints[1]));
-                if (person != null) {
-                    person.setName(newPerson.getName());
-                    personService.updatePerson(person);
-                    response.setStatus(HttpServletResponse.SC_OK);
-                }
-                else {
-                    response.sendError(HttpServletResponse.SC_NOT_FOUND);
-                }
+        List<String> url = getPathInfo(request);
+        if (url == null) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+        else if (url.size() == 1) {
+            BufferedReader reader = request.getReader();
+            String json = readJsonData(reader);
+            Person newPerson = objectMapper.readValue(json, Person.class);
+            Person person = personService.getPersonById(Long.parseLong(url.get(0)));
+            if (person != null) {
+                person.setName(newPerson.getName());
+                personService.updatePerson(person);
+                response.setStatus(HttpServletResponse.SC_OK);
             }
             else {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
             }
         }
         else {
-            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
         }
     }
 
     @Override
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String pathInfo = request.getPathInfo();
-        if (pathInfo != null) {
-            String[] endpoints = pathInfo.split("/");
-            if (endpoints.length != 2) {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-            }
-            else if (StringUtils.isNumeric(endpoints[1])) {
-                personService.deletePersonById(Long.parseLong(endpoints[1]));
-                response.setStatus(HttpServletResponse.SC_OK);
-            }
-            else {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-            }
+        List<String> url = getPathInfo(request);
+        if (url == null) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+        else if (url.size() == 1) {
+            personService.deletePersonById(Long.parseLong(url.get(0)));
+            response.setStatus(HttpServletResponse.SC_OK);
         }
         else {
-            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
         }
-    }
-
-    private String readJsonData(BufferedReader reader) throws IOException {
-        StringBuilder buffer = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            buffer.append(line);
-        }
-        return buffer.toString();
     }
 }
--- src/main/java/com/leverx/pets/controller/servlet/PetServlet.java
+++ src/main/java/com/leverx/pets/controller/servlet/PetServlet.java
@@ -17,12 +17,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
 import static com.leverx.pets.model.PetType.CAT;
+import static com.leverx.pets.util.JsonUtil.readJsonData;
+import static com.leverx.pets.util.JsonUtil.sendJsonResponse;
+import static com.leverx.pets.util.UrlParser.getPathInfo;
 
-@WebServlet("/pets/*")
+@WebServlet(value = "/pets/*", name = "PetServlet")
 public class PetServlet extends HttpServlet {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
@@ -38,73 +40,70 @@ public class PetServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String pathInfo = request.getPathInfo();
-        if (pathInfo == null || pathInfo.equals("/")){
+        List<String> url = getPathInfo(request);
+        if (url == null) {
             List<Pet> pets = petService.getAllPets();
-
-            response.setContentType("application/json");
-            response.setCharacterEncoding("UTF-8");
-            String jsonPets = objectMapper.writeValueAsString(pets);
-            PrintWriter out = response.getWriter();
-            out.print(jsonPets);
-            out.flush();
+            String jsonPeople = objectMapper.writeValueAsString(pets);
+            sendJsonResponse(jsonPeople, response);
         }
-        else {
-            String[] endpoints = pathInfo.split("/");
-            if (endpoints.length != 2) {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-            }
-            else if (StringUtils.isNumeric(endpoints[1])) {
-                Pet pet = petService.getPetById(Long.parseLong(endpoints[1]));
-                if (pet != null) {
-                    response.setContentType("application/json");
-                    response.setCharacterEncoding("UTF-8");
-                    String jsonPet = objectMapper.writeValueAsString(pet);
-
-                    PrintWriter out = response.getWriter();
-                    out.print(jsonPet);
-                    out.flush();
-                }
-                else {
-                    response.sendError(HttpServletResponse.SC_NOT_FOUND);
-                }
+        else if (url.size() == 1) {
+            Pet pet = petService.getPetById(Long.parseLong(url.get(0)));
+            if (pet != null) {
+                String jsonPet = objectMapper.writeValueAsString(pet);
+                sendJsonResponse(jsonPet, response);
             }
             else {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
             }
+        }
+        else {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
         }
     }
 
     // pets/person/{id}
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String pathInfo = request.getPathInfo();
+        List<String> url = getPathInfo(request);
 
-        if (pathInfo != null) {
-            String[] endpoints = pathInfo.split("/");
-            if (endpoints.length != 3) {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        if (url != null && url.size() == 2) {
+            BufferedReader reader = request.getReader();
+            String json = readJsonData(reader);
+            PetDto petDto = objectMapper.readValue(json, PetDto.class);
+            Pet pet;
+            if (CAT.equals(petDto.getPetType())) {
+                pet = new Cat();
             }
-            else if (StringUtils.isNumeric(endpoints[2]) && "person".equals(endpoints[1])) {
-                BufferedReader reader = request.getReader();
-                String json = readJsonData(reader);
-                PetDto petDto = objectMapper.readValue(json, PetDto.class);
-                Pet pet;
-                if (CAT.equals(petDto.getPetType())) {
-                    pet = new Cat();
-                }
-                else {
-                    pet = new Dog();
-                }
-                Person person = personService.getPersonById(Long.parseLong(endpoints[2]));
-                if (person != null) {
-                    pet.setName(petDto.getName());
-                    pet.setPerson(person);
-                }
-                else {
-                    response.sendError(HttpServletResponse.SC_NOT_FOUND);
-                }
-                petService.createPet(pet);
+            else {
+                pet = new Dog();
+            }
+            Person person = personService.getPersonById(Long.parseLong(url.get(1)));
+            if (person != null) {
+                pet.setName(petDto.getName());
+                pet.setPerson(person);
+            }
+            else {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            }
+            petService.createPet(pet);
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+        else {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        List<String> url = getPathInfo(request);
+        if (url != null && url.size() == 1) {
+            BufferedReader reader = request.getReader();
+            String json = readJsonData(reader);
+            PetDto petDto = objectMapper.readValue(json, PetDto.class);
+            Pet pet = petService.getPetById(Long.parseLong(url.get(0)));
+            if (pet != null) {
+                pet.setName(petDto.getName());
+                petService.updatePet(pet);
                 response.setStatus(HttpServletResponse.SC_OK);
             }
             else {
@@ -116,65 +115,16 @@ public class PetServlet extends HttpServlet {
         }
     }
 
-    @Override
-    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String pathInfo = request.getPathInfo();
-        if (pathInfo != null) {
-            String[] endpoints = pathInfo.split("/");
-            if (endpoints.length != 2) {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-            }
-            else if (StringUtils.isNumeric(endpoints[1])) {
-                BufferedReader reader = request.getReader();
-                String json = readJsonData(reader);
-                PetDto petDto = objectMapper.readValue(json, PetDto.class);
-                Pet pet = petService.getPetById(Long.parseLong(endpoints[1]));
-                if (pet != null) {
-                    pet.setName(petDto.getName());
-                    petService.updatePet(pet);
-                    response.setStatus(HttpServletResponse.SC_OK);
-                }
-                else {
-                    response.sendError(HttpServletResponse.SC_NOT_FOUND);
-                }
-            }
-            else {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-            }
-        }
-        else {
-            response.sendError(HttpServletResponse.SC_NOT_FOUND);
-        }
-    }
-
     // pets/{id}
     @Override
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String pathInfo = request.getPathInfo();
-        if (pathInfo != null) {
-            String[] endpoints = pathInfo.split("/");
-            if (endpoints.length != 2) {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-            }
-            else if (StringUtils.isNumeric(endpoints[1])) {
-                petService.deletePet(Long.parseLong(endpoints[1]));
-                response.setStatus(HttpServletResponse.SC_OK);
-            }
-            else {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-            }
+        List<String> url = getPathInfo(request);
+        if (url != null && url.size() == 1) {
+            petService.deletePet(Long.parseLong(url.get(0)));
+            response.setStatus(HttpServletResponse.SC_OK);
         }
         else {
-            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
         }
-    }
-
-    private String readJsonData(BufferedReader reader) throws IOException {
-        StringBuilder buffer = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            buffer.append(line);
-        }
-        return buffer.toString();
     }
 }
